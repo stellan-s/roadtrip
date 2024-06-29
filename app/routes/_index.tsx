e691e4edf03482a143b3bf31ebc7eb6ce2f2d339
stellan-s <stellan.sarnblom@amandaai.com>
2024-06-29 14:33:32 +0200

    Init

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,27 +13,7 @@ export default function Index() {
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+         nothing
         </li>
       </ul>
     </div>
