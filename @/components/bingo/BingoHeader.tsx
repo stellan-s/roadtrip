40cfe10efcdda2e5ef01a6ba370d1dc8bcf7fb19
stellan-s <stellan.sarnblom@amandaai.com>
2025-06-02 20:00:22 +0200

    fix: adjust text size and shadow effect in BingoHeader component for improved visibility

--- @/components/bingo/BingoHeader.tsx
+++ @/components/bingo/BingoHeader.tsx
@@ -1,7 +1,7 @@
 export function BingoHeader({ textColorClass }: { textColorClass: string }) {
   return (
     <h1
-      className={`max-w-[500px] text-center text-4xl uppercase md:text-6xl text-pretty break-words font-bold tracking-wider drop-shadow-lg ${textColorClass}`}
+      className={`max-w-[500px] text-center text-xl uppercase md:text-6xl text-pretty break-keep  md:break-words font-bold tracking-wider drop-shadow-2xl ${textColorClass}`}
     >
       Roadtrip Bingo 3000
     </h1>
