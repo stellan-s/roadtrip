cec23c780fb6e2a78b44b78cd2339779635c7071
stellan-s <stellan.sarnblom@amandaai.com>
2026-02-01 09:32:19 +0100

    fix: update HTML and body styles for consistent background color and gradient

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" style={{ backgroundColor: "#12c2e9" }}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -27,7 +27,8 @@ export default function App() {
       <body
         className="overflow-y-auto"
         style={{
-          background: "linear-gradient(to bottom, #12c2e9, #c471ed, #f64f59)",
+          backgroundColor: "#12c2e9",
+          backgroundImage: "linear-gradient(to bottom, #12c2e9, #c471ed, #f64f59)",
         }}
       >
         <Outlet />
